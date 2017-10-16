01718b90a4f7a51369a70bfe16585343e2d5a2d5
tristanhamel <tristan.hamel@kielikone.fi>
2017-10-16 16:03:38 -0400

    quizz component

--- src/components/quizz/quizz-container.vue
+++ src/components/quizz/quizz-container.vue
@@ -1,23 +1,42 @@
 <template>
   <div>
-    <multiple-card :question="question"
-                   @onAnswer="onAnswer"
-                   v-if="question.type === 'multiple'">
-    </multiple-card>
-    <boolean-card :question="question"
-                   @onAnswer="onAnswer"
-                   v-if="question.type === 'boolean'">
-    </boolean-card>
+    <div>
+      <div>{{ question.category }}</div>
+      <div>
+        <span v-for="i in question.difficulty">*</span>
+        <span v-for="i in 3 - question.difficulty">-</span>
+      </div>
+    </div>
+    <div>
+      {{ question.question }}
+    </div>
+    <div>
+      <form>
+        <span v-for="a in question.answers">
+          <input v-model="answer"
+                 type="radio"
+                 :id="a.text"
+                 @click="onAnswer(a.correct)"
+                 :value="a.text">
+          <label :for="a.text">
+            {{ a.text }}
+          </label>
+        </span>
+      </form>
+      <div>
+        <button @click="onAnswer('pass')">
+          Pass
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   import Vue from 'vue';
 
-  import multipleCard from './multiple-card.vue';
-  import booleanCard from './boolean-card.vue';
-
   export default Vue.component('quizz-container', {
+    data: () => ({answer: ''}),
     props: {
       question: Object
     },
@@ -26,12 +45,9 @@
         this.$emit('onAnswer', correct);
       },
     },
-    components: {
-      multipleCard,
-      booleanCard
-    },
+    components: {},
     beforeUpdate() {
-      console.log(this.question);
+//      this.answer = '';
     }
   })
 </script>

b294844aa4c046fb988558c12aca6a1061cfaa33
tristanhamel <tristan.hamel@kielikone.fi>
2017-10-19 16:07:40 -0400

    gh-pages trim

--- src/settings.js
+++
@@ -1,5 +0,0 @@
-export const STAGGERING_DELAY = 500; // for counter adding points and tiles illuminating after an answer
-export const NEXT_QUESTION_DELAY = 700; // for moving on to next question after having added points
-export const ANSWER_RESOLUTION_DELAY = 500; // anim question cards with right/wrong answer
-export const TILES_STAGGER_DELAY = 80;
-export const FALLING_TILES_INTERVAL = 100;
